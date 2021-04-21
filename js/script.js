26b001c314a9cc543a3a31de81276c0ffd73545b
Greatest Lord <greatestlords@gmail.com>
2021-04-21 20:22:40 +0530

    Added dark mode switch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
-// Accordion sub menu controller
+
+// Dark Mode Toggle
+
+let theme = localStorage.getItem('theme');
+const darkModeToggle = document.querySelector('#darkModeToggle');
+
+let switchToDarkTheme = () => {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	localStorage.setItem('theme', 'dark');
+	
+	if (darkModeToggle.checked === false) {
+		darkModeToggle.checked = true;
+	}
+}
+
+let switchToLightTheme = () => {
+	document.documentElement.setAttribute('data-theme', 'light');
+	localStorage.setItem('theme', 'light');
+}
+
+if (theme === 'dark') {
+	switchToDarkTheme();
+}
+
+darkModeToggle.addEventListener('change', () => {
+	if (darkModeToggle.checked) {
+		switchToDarkTheme();
+	} else {
+		switchToLightTheme();
+	}
+});
+
+// Accordion - aside sub menu controller
 
 var accordion = document.querySelectorAll(".accordion");
 
